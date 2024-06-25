f02a9f9ef5fcc8ecacb5b001599fd814ff51b5c8
mariabrum27 <mariaisabel.brum27@gmail.com>
2024-06-24 23:16:45 -0500

    Updated comments

--- cypress/integration/examples/smoketest.spec.js
+++ cypress/integration/examples/smoketest.spec.js
@@ -47,123 +47,123 @@ context('smoketest', () => {
       })
     })
   
-    // describe('Explicit Assertions', () => {
-    //   // https://on.cypress.io/assertions
-    //   it('expect - make an assertion about a specified subject', () => {
-    //     // We can use Chai's BDD style assertions
-    //     expect(true).to.be.true
-    //     const o = { foo: 'bar' }
+    describe('Explicit Assertions', () => {
+      // https://on.cypress.io/assertions
+      it('expect - make an assertion about a specified subject', () => {
+        // We can use Chai's BDD style assertions
+        expect(true).to.be.true
+        const o = { foo: 'bar' }
   
-    //     expect(o).to.equal(o)
-    //     expect(o).to.deep.equal({ foo: 'bar' })
-    //     // matching text using regular expression
-    //     expect('FooBar').to.match(/bar$/i)
-    //   })
+        expect(o).to.equal(o)
+        expect(o).to.deep.equal({ foo: 'bar' })
+        // matching text using regular expression
+        expect('FooBar').to.match(/bar$/i)
+      })
   
-    //   it('pass your own callback function to should()', () => {
-    //     // Pass a function to should that can have any number
-    //     // of explicit assertions within it.
-    //     // The ".should(cb)" function will be retried
-    //     // automatically until it passes all your explicit assertions or times out.
-    //     cy.get('.assertions-p')
-    //       .find('p')
-    //       .should(($p) => {
-    //         // https://on.cypress.io/$
-    //         // return an array of texts from all of the p's
-    //         // @ts-ignore TS6133 unused variable
-    //         const texts = $p.map((i, el) => Cypress.$(el).text())
+      it('pass your own callback function to should()', () => {
+        // Pass a function to should that can have any number
+        // of explicit assertions within it.
+        // The ".should(cb)" function will be retried
+        // automatically until it passes all your explicit assertions or times out.
+        cy.get('.assertions-p')
+          .find('p')
+          .should(($p) => {
+            // https://on.cypress.io/$
+            // return an array of texts from all of the p's
+            // @ts-ignore TS6133 unused variable
+            const texts = $p.map((i, el) => Cypress.$(el).text())
   
-    //         // jquery map returns jquery object
-    //         // and .get() convert this to simple array
-    //         const paragraphs = texts.get()
+            // jquery map returns jquery object
+            // and .get() convert this to simple array
+            const paragraphs = texts.get()
   
-    //         // array should have length of 3
-    //         expect(paragraphs, 'has 3 paragraphs').to.have.length(3)
+            // array should have length of 3
+            expect(paragraphs, 'has 3 paragraphs').to.have.length(3)
   
-    //         // use second argument to expect(...) to provide clear
-    //         // message with each assertion
-    //         expect(paragraphs, 'has expected text in each paragraph').to.deep.eq([
-    //           'Some text from first p',
-    //           'More text from second p',
-    //           'And even more text from third p',
-    //         ])
-    //       })
-    //   })
+            // use second argument to expect(...) to provide clear
+            // message with each assertion
+            expect(paragraphs, 'has expected text in each paragraph').to.deep.eq([
+              'Some text from first p',
+              'More text from second p',
+              'And even more text from third p',
+            ])
+          })
+      })
   
-    //   it('finds element by class name regex', () => {
-    //     cy.get('.docs-header')
-    //       .find('div')
-    //       // .should(cb) callback function will be retried
-    //       .should(($div) => {
-    //         expect($div).to.have.length(1)
+      it('finds element by class name regex', () => {
+        cy.get('.docs-header')
+          .find('div')
+          // .should(cb) callback function will be retried
+          .should(($div) => {
+            expect($div).to.have.length(1)
   
-    //         const className = $div[0].className
+            const className = $div[0].className
   
-    //         expect(className).to.match(/heading-/)
-    //       })
-    //       // .then(cb) callback is not retried,
-    //       // it either passes or fails
-    //       .then(($div) => {
-    //         expect($div, 'text content').to.have.text('Introduction')
-    //       })
-    //   })
+            expect(className).to.match(/heading-/)
+          })
+          // .then(cb) callback is not retried,
+          // it either passes or fails
+          .then(($div) => {
+            expect($div, 'text content').to.have.text('Introduction')
+          })
+      })
   
-    //   it('can throw any error', () => {
-    //     cy.get('.docs-header')
-    //       .find('div')
-    //       .should(($div) => {
-    //         if ($div.length !== 1) {
-    //           // you can throw your own errors
-    //           throw new Error('Did not find 1 element')
-    //         }
+      it('can throw any error', () => {
+        cy.get('.docs-header')
+          .find('div')
+          .should(($div) => {
+            if ($div.length !== 1) {
+              // you can throw your own errors
+              throw new Error('Did not find 1 element')
+            }
   
-    //         const className = $div[0].className
+            const className = $div[0].className
   
-    //         if (!className.match(/heading-/)) {
-    //           throw new Error(`Could not find class "heading-" in ${className}`)
-    //         }
-    //       })
-    //   })
+            if (!className.match(/heading-/)) {
+              throw new Error(`Could not find class "heading-" in ${className}`)
+            }
+          })
+      })
   
-    //   it('matches unknown text between two elements', () => {
-    //     /**
-    //      * Text from the first element.
-    //      * @type {string}
-    //     */
-    //     let text
+      it('matches unknown text between two elements', () => {
+        /**
+         * Text from the first element.
+         * @type {string}
+        */
+        let text
   
-    //     /**
-    //      * Normalizes passed text,
-    //      * useful before comparing text with spaces and different capitalization.
-    //      * @param {string} s Text to normalize
-    //     */
-    //     const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase()
+        /**
+         * Normalizes passed text,
+         * useful before comparing text with spaces and different capitalization.
+         * @param {string} s Text to normalize
+        */
+        const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase()
   
-    //     cy.get('.two-elements')
-    //       .find('.first')
-    //       .then(($first) => {
-    //         // save text from the first element
-    //         text = normalizeText($first.text())
-    //       })
+        cy.get('.two-elements')
+          .find('.first')
+          .then(($first) => {
+            // save text from the first element
+            text = normalizeText($first.text())
+          })
   
-    //     cy.get('.two-elements')
-    //       .find('.second')
-    //       .should(($div) => {
-    //         // we can massage text before comparing
-    //         const secondText = normalizeText($div.text())
+        cy.get('.two-elements')
+          .find('.second')
+          .should(($div) => {
+            // we can massage text before comparing
+            const secondText = normalizeText($div.text())
   
-    //         expect(secondText, 'second text').to.equal(text)
-    //       })
-    //   })
+            expect(secondText, 'second text').to.equal(text)
+          })
+      })
   
-    //   it('assert - assert shape of an object', () => {
-    //     const person = {
-    //       name: 'Joe',
-    //       age: 20,
-    //     }
+      it('assert - assert shape of an object', () => {
+        const person = {
+          name: 'Joe',
+          age: 20,
+        }
   
-    //     assert.isObject(person, 'value is object')
-    //   })
-    // })
+        assert.isObject(person, 'value is object')
+      })
+    })
   })
   
