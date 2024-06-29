e88443acb5de9a9ab6716851a78c7ea2d9d98884
mariabrum27 <mariaisabel.brum27@gmail.com>
2024-06-28 19:06:34 -0500

    disable comment login test

--- cypress/integration/examples/smoketest.spec.js
+++ cypress/integration/examples/smoketest.spec.js
@@ -8,28 +8,28 @@ context('smoketest', () => {
   })
 
   // Define the test case to validate successful login
-  // describe('Validate successful login', () => {
-  //   it('verify user is able to login with valid credentials', () => {
+  describe('Validate successful login', () => {
+    it('verify user is able to login with valid credentials', () => {
 
-  //     // Type the username 'visual_user' in the username field
-  //     cy.get('#user-name').type('visual_user')
-  //     // Type the password 'secret_sauce' in the password field
-  //     cy.get('#password').type('secret_sauce')
-  //     // Click on the login button
-  //     cy.get('#login-button').click()
+      // Type the username 'visual_user' in the username field
+      cy.get('#user-name').type('visual_user')
+      // Type the password 'secret_sauce' in the password field
+      cy.get('#password').type('secret_sauce')
+      // Click on the login button
+      cy.get('#login-button').click()
 
-  //     // Check if the URL includes 'inventory.html' after successful login
-  //     cy.url().should('include', 'inventory.html');
+      // Check if the URL includes 'inventory.html' after successful login
+      cy.url().should('include', 'inventory.html');
 
-  //     // Verify that there is an expected element on the main page after successful login
-  //     cy.get('#react-burger-menu-btn').click()
-  //     cy.get('#inventory_sidebar_link').should('have.text', 'All Items') // Check for 'All Items' text
-  //     cy.get('#about_sidebar_link').should('have.text', 'About') // Check for 'About' text
-  //     cy.get('#logout_sidebar_link').should('have.text', 'Logout') // Check for 'Logout' text
-  //     cy.get('#reset_sidebar_link').should('have.text', 'Reset App State') // Check for 'Reset App State' text
+      // Verify that there is an expected element on the main page after successful login
+      cy.get('#react-burger-menu-btn').click()
+      cy.get('#inventory_sidebar_link').should('have.text', 'All Items') // Check for 'All Items' text
+      cy.get('#about_sidebar_link').should('have.text', 'About') // Check for 'About' text
+      cy.get('#logout_sidebar_link').should('have.text', 'Logout') // Check for 'Logout' text
+      cy.get('#reset_sidebar_link').should('have.text', 'Reset App State') // Check for 'Reset App State' text
 
-  //   })
-  // })
+    })
+  })
 
   // Define the test case to validate failed login
   describe('Validate failed login', () => {
