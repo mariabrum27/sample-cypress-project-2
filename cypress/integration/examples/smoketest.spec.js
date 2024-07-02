d88c280101668fe9d8c2767fd623143d2958827b
mariabrum27 <mariaisabel.brum27@gmail.com>
2024-07-02 16:02:42 -0500

    new changes

--- cypress/integration/examples/smoketest.spec.js
+++ cypress/integration/examples/smoketest.spec.js
@@ -5,7 +5,7 @@ context('successful login', () => {
     cy.visit('https://www.saucedemo.com/')
   });
   describe('validate that the user is able to login by entering the correct credentials', () => {
-    it('', () => {
+    it('Validate successful login', () => {
       //verify the username field
       cy.get('#user-name').should('exist')
       cy.get('#user-name').type('standard_user')
